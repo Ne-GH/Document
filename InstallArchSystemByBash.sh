2da6079ff32242e905cec9dcd6687118f42f55b3
Ne-GH <printfne@gmail.com>
2022-03-02 23:09:04 +0800

    Install Arch System By Bash

---
+++ InstallArchSystemByBash.sh
@@ -0,0 +1,24 @@
+START(){
+    echo "This is funtion START"
+
+
+}
+
+NOSTART(){
+    echo "Bye"
+}
+
+
+Main(){
+    echo -en "I always install arch,but i haven't more time,so write this bash.\nThis bash will think your install system desk is /dev/sdb2,if you shuld modify this,please modify this bash file.If you read all tips ,Please confirm to continue.[Y/N]"
+    read ConfirmStart
+    case "$ConfirmStart" in
+        Y|y|YES|Yes|yes)
+            START ;;
+        *)
+            NOSTART ;;
+    esac
+}
+Main
+
+
