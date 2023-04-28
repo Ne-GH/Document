02cd99d7cc01672aef8a5ef059ffdc4e5be643de
Ne-GH <printfne@gmail.com>
2023-04-28 13:53:54 +0800

    修正超时版本

--- CSAPP/data_lab/bitXor.c
+++ CSAPP/data_lab/bitXor.c
@@ -9,6 +9,7 @@ int bitXor(int x,int y) {
 
 /*
  * 提示超时版本
+ * 更新：超时是因为用于debug的printf被多次调用，注释即可
  */
 int bitXor_time_out(int x,int y) {
    int num1 = (~x & y);
@@ -16,6 +17,6 @@ int bitXor_time_out(int x,int y) {
 
    int num3 = ~(num1 & num2);
    int num4 = (~num1 & ~num2);
-   printf("%d %d %d %d\n",num1,num2,num3,num4);
+   // printf("%d %d %d %d\n",num1,num2,num3,num4);
    return ~(num3 & num4);
 }
