ccd930d053cbcba1c1a43b04aa56681064e1ae47
永恒之蓝 <61956889+Ne-GH@users.noreply.github.com>
2024-02-20 14:58:19 +0800

    补充union的用法及注意事项

--- CPP/union.cpp
+++ CPP/union.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
 
-using namespace std;
-
-
+// 全局的匿名union必须为static
 static union {
     char C;
     int I;
     double D;
 };
 
-union Union{
-    Union(){
-        cout << "Union" << endl;
+// union可以拥有构造函数和析构函数，但是不能作为基类，不能有虚函数，不能有引用类型的成员，不能有protected 成员或 private 成员
+union Union {
+    Union() {
+        std::cout << "Union" << std::endl;
     }
-    ~Union(){
-        cout << "~Union" << endl;
+    ~Union() {
+        std::cout << "~Union" << std::endl;
     }
 
     int INT;
     double Double;
 };
-int main(){
+int main() {
 
     Union U;
     U.INT = 10;
-    cout << U.INT << endl;
+    std::cout << U.INT << std::endl;
+
+    union {
+        char C;
+        int I;
+        double D;
+    };
+
+
+    // 全局匿名union中的I
+    ::I = 10;
+    std::cout << ::I << std::endl;
+
+    // 当前作用域中匿名union中的I
+    I = 20;
+    std::cout << I << std::endl;
 
     return 0;
 }
