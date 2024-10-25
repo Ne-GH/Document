bdf7b45bdcfc76476d9fc0510b468d96dee776ff
ne-gh <printfne@gmail.com>
2024-10-25 17:09:26 +0800

    修复该文件命名错误，以及部分头文件以外引入

--- CPP/TMP/binary_to_decimal.cpp
+++ CPP/TMP/binary_to_decimal.cpp
@@ -1,20 +1,13 @@
-#include <chrono>
-#include <thread>
-#include <string>
-#include <fstream>
-#include <iterator>
-
-#include <QPlainTextEdit>
-#include <opencv2/opencv.hpp>
+#include <iostream>
 
 // 传入的是一个二进制形式的10进制数字,例如，1101,该int数字被认为是二进制形式的
 template <int N>
 struct Binary {
-    inline static int value = Binary<N/10>::value * 2 + N % 10;
+    static constexpr int value = Binary<N/10>::value * 2 + N % 10;
 };
 template<>
 struct Binary<0> {
-    inline static int value = 0;
+    static constexpr int value = 0;
 };
 
 int main() {
