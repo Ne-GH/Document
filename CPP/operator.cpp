b620f8e84977fb5ded9c3bd3713107345b4fd970
ne-gh <printfne@gmail.com>
2024-10-28 23:00:02 +0800

    重构opertor.cpp

--- CPP/operator.cpp
+++ CPP/operator.cpp
@@ -1,228 +1,171 @@
-#include <fstream>
 #include <iostream>
-#include <istream>
-#include <ostream>
+#include <vector>
+#include <ranges>
+#include <algorithm>
+/*******************************************************************************
+* 重载运算符应被定义为普通非成员函数还是成员函数规则,如下:
+* 赋值(=),下标([]),调用(()),和成员访问箭头(->)运算符必须是成员
+* 复合赋值运算符一般应该是成员,但是并非必须~
+* 改变对象状态的运算符或者与给定类型密切相关的运算符,自增++,自减--,解引用*,成员访问运算符.通常应该是成员函数
+* 具有对称性的运算符可以转换到任意一端的运算对象,例如算术,相等性,关系和位运算符等,因此他们通常应该是普通的非成员函数
+*******************************************************************************/
 
-
-using std::cout,std::endl;
-// 重载的运算符的本质是函数
-// 我们不能重载内置类型的算数运算符
-// 因此我们在这里构造我们自己的类
-
-
-// 重载运算符应被定义为普通非成员函数还是成员函数规则,如下:
-// 赋值(=),下标([]),调用(()),和成员访问箭头(->)运算符必须是成员
-// 复合赋值运算符一般应该是成员,但是并非必须~
-// 改变对象状态的运算符或者与给定类型密切相关的运算符,自增++,自减--,解引用*,成员访问运算符.通常应该是成员函数
-// 具有对称性的运算符可以转换到任意一端的运算对象,例如算术,相等性,关系和位运算符等,因此他们通常应该是普通的非成员函数
-class node{
+class Person {
+    int age_{};
+    std::string name_;
 public:
-    int id;
-    int spend;
-    
-    // 构造
-    node(){
-        id = 0;
-        spend = 0;
-        for(int i = 0;i < 10; ++i){
-            nums[i] = i;
-        }
+    Person(const int age, const std::string_view name) : age_(age), name_(name) {
+        std::cout << "构造函数" << std::endl;
+    }
+    ~Person() {
+        std::cout << "析构函数" << std::endl;
     }
 
-    // 我们应该避免除了向bool显式转换以外的转换
-    operator bool() const{
-        if(id != 0)
-            return true;
-        return false;
-    }
+    Person(const Person &other) : age_(other.age_), name_(other.name_) {  }
 
-    node operator += (node no){
-        node temp;
-        temp.spend = this->spend + no.spend;
-        return temp;
-    }
-    node operator -= (node no){
-        node temp;
-        temp.spend = this->spend - no.spend;
-        return temp;
-    }
-    int operator [] (int num){
-        return this->nums[num];
-    }
-    // 虽然此处未返回引用,但是由于返回了指针,因此我们的修改也会在内存中生效
-    int* operator *(){
-        return (*this).nums;
-    }
-    // c++ pirmer 中提到重载的->必须返回类的指针或者自定义了箭头运算符的某个类的对象,但是我们这里返回的显然与此描述不符
-    int* operator -> (){
-        return this->nums;
-    }
-    int operator ()(){
-        return id;
-    }
-    // 前置
-    node& operator ++(){
-        this->id ++;
+    Person(Person &&other) noexcept : age_(other.age_), name_(std::move(other.name_)) {  }
+
+    Person &operator = (const Person &other) {
+        age_ = other.age_;
+        name_ = other.name_;
         return *this;
     }
-    // 后置
-    node operator ++(int){
-        node no = *this;
-        this->id ++;
-        return no;
-    }
-    
-    
-    void* operator new(unsigned long size_t){
-        cout << "重载的new" << endl;
 
-        if(void *p = malloc(size_t)){
-            return p;
+    Person &operator = (Person &&other) noexcept {
+        age_ = other.age_;
+        name_ = std::move(other.name_);
+        return *this;
+    }
+
+
+    explicit operator std::string() const {
+        return name_;
+    }
+    explicit operator int() const {
+        return age_;
+    }
+
+    Person &operator ++ () {
+        ++age_;
+        return *this;
+    }
+    Person operator ++ (int) {
+        Person tmp = *this;
+        ++age_;
+        return tmp;
+    }
+    Person &operator -- () {
+        --age_;
+        return *this;
+    }
+    Person operator -- (int) {
+        Person tmp = *this;
+        -- age_;
+        return tmp;
+    }
+
+    Person &operator * () {
+        return *this;
+    }
+    Person * operator -> () {
+        return this;
+    }
+
+    // + - += -= () [],实现略过,此处仅为展示delete使用
+    void operator +  (const Person&) = delete;
+    void operator -  (const Person&) = delete;
+    void operator += (const Person&) = delete;
+    void operator -= (const Person&) = delete;
+
+
+    void* operator new (const size_t size) {
+        auto p = static_cast<Person *>(::malloc(size));
+        if (p == nullptr) {
+            std::get_new_handler()();
+            throw std::bad_alloc();
         }
-        else
-            return NULL;
+        return p;
     }
-
-    void operator delete(void *p){
+    void operator delete(void *p) {
+        ::free(p);
+        return;
+    }
+    void* operator new[] (const size_t size) {
+        auto p = static_cast<Person *>(::malloc(size));
+        if (p == nullptr) {
+            std::get_new_handler()();
+            throw std::bad_alloc();
+        }
+        return p;
+    }
+    void operator delete[] (void *p) {
         free(p);
-    }
-
-    // const 对象的重载
-    void show() const {
-        std::cout << "const object" << std::endl;
-    }
-    void show() {
-        std::cout << "object" << std::endl;
-    }
-
-    void show_() & {
-        std::cout << "左值" << std::endl;
-    }
-    void show_() && {
-        std::cout << "右值" << std::endl;
+        return;
     }
 
 
-private:
-    int nums[10];
 
 
+    void show() & {
+        std::cout << "&" << std::endl;
+    }
+    void show() && {
+        std::cout << "&&" << std::endl;
+    }
+    void show() const & {
+        std::cout << "const &" << std::endl;
+    }
+    void show() const && {
+        std::cout << "const &&" << std::endl;
+    }
 
-    // 当我们把一个运算符定义为成员函数时,他的左侧对象类型应与重载该符号的类的类型相同
-    
-     
-};
+    friend std::ostream &operator << (std::ostream &os, const Person &p) {
+        os << p.age_ << ' ' << p.name_;
+        return os;
+    }
 
-// 非成员函数类型的重载
-// 正常情况下我们应该只输入输出,不进行格式控制,这里算是一个反面教材
-std::istream& operator >> (std::istream &in,node &no){
-    std::cout << "please input node id:";
-    in >> no.id;
-    std::cout << "please input node spend:";
-    in >> no.spend;
-    return in;
-}
-// 输出时node 不必为引用类型,否则不可以cout << no1 + no2 这种样式输出
-// 因为no1 + no2运算后为一个右值,对于一个右值自然是不能引用的
-std::ostream& operator << (std::ostream &out,node no){
-    std::cout << no.id << " ";
-    std::cout << no.spend;
-    return out;
-}
-node operator + (node no1,node no2){
-    node temp;
-    temp.spend += no1.spend;
-    temp.spend += no2.spend;
-    return temp;
-}
-node operator - (node no1,node no2){
-    node temp;
-    temp.spend = no1.spend - no2.spend;
-    return temp;
-}
-bool operator == (node no1,node no2){
-    if(no1.id == no2.id)
-        return true;
-    return false;
-}
-bool operator != (node no1,node no2){
-    if(no1.id != no2.id)
-        return true;
-    return false;
-}
-// 返回类型不一定需要是node,但是参数列表必须如此
-node operator ""_ms(const char *str,std::size_t len){
-    // ... 使用的时候就是"1233"_ms
-    return node();
-}
 
-class Node {
-    int _val;
-public:
-    // <=>重载仅会生成<,<=,>,>=
-    // 对于!=，如果==没有被定义，则!=默认是delete的，否则为==的取反
-  
-    // 强序
-    // auto operator <=> (const Node& node1){
-    //     return std::strong_ordering (_val <=> node1._val);
-    // }
-
-    // std::strong_ordering operator <=> (node node1,node node2) {
-    //     if (node1 == node2) {
-    //       return std::strong_ordering::equal;
-    //     }
-    //     else if (node1 < node2) {
-    //         return std::strong_ordering::less;
-    //     }
-    //     else {
-    //        return std::strong_ordering::greater;
-    //     }
-    // }
-    
-    // 弱序
-    auto operator <=> (const Node& node1) {
-        return std::weak_ordering (_val <=> node1._val);
+    /*******************************************************************************
+      强序（Strong Ordering）: 对于任意两个元素 a 和 b，如果 a < b，则不可能有 b < a。
+      弱序（Weak Ordering）: 可能存在 a < b 和 b < a 的情况。在这种情况下，两个元素被认为是等价的，但不一定是相等的。
+    *******************************************************************************/
+    /*******************************************************************************
+      如果定义了 operator==，那么编译器会自动生成不等运算符。
+      类似地，如果定义了三路比较运算符 operator<=>，那么编译器会自动生成四个关系运算符, < <= > >=
+      如果定义 operator<=> 为default ，那么编译器会生成 operator== 和 operator<=>
+    *******************************************************************************/
+    auto operator <=> (const Person &other) const {
+        if (const auto cmp = std::strong_order(age_, other.age_); cmp != nullptr)
+            return cmp;
+        return std::strong_order(other.name_, name_);
+    }
+    bool operator == (const Person &other) const {
+        return name_ == other.name_ && age_ == other.age_;
     }
 };
-int main(){
-    node no1,no2;
-    // 如果把node的输入输出定义为成员函数,则如下:
-    // no1 >> cin;      因为成员函数运算符的左侧为他自身,唯一的参数为运算符右侧的符号
 
-    // 输入输出
-    std::cin >> no1 >> no2;
-    std::cout << no1 << std::endl << no2 << std::endl;
-    // +
-    std::cout << no1+no2 << std::endl;
-    // -
-    std::cout << no1-no2 << std::endl;
-    
-    // ==
-    node no3 = no1 + no2;
-    if(no3 == no1 + no2)
-        std::cout << "True" << std::endl;
+Person operator ""_person(const char *str,std::size_t len){
+    // ... 使用的时候就是"xiaoming"_person
+    return Person(0,str);
+}
 
-    // []
-    std::cout << no1[3] << std::endl;
 
-    // ++
-    node no4 = ++ no1;
-    std::cout << no4 << std::endl;
-    no4 = no1 ++;
-    std::cout << no4 << std::endl;
+int main () {
 
-    // *
-    (*no1)[5] = 10;
-    std::cout << (*no1)[5] << " " ;
-    // ->     
-    (*no1)[5] = 5;
-    std::cout << no1.operator->()[5] << std::endl;
 
-    // () 与构造函数有区别
-    std::cout << no2.id << " " << no2() << std::endl;
+    std::vector<Person> vec{
+        {2,"x"},
+        {2,"xiaoming"},
+        {1,"John"}
+    };
 
-    // int()
-    std::cout << no2 << " " << (bool)no2 << std::endl;
+    std::ranges::sort(vec);
+    for (const auto & person : vec) {
+        std::cout << person << '\n';
+    }
+
+
+
 
     return 0;
 }
